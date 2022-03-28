e1cb5bcd20a1f666cd706fff9115407a3458177b
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-03-28 16:20:31 +0200

    add Main Footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <!-- header -->
-    <eader>
+    <header>
       <Header />
-    </eader>
+    </header>
 
     <!-- main -->
     <main>
@@ -19,16 +19,19 @@
 
 <script>
 import Header from "./components/Header.vue";
-
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 export default {
   name: "App",
   components: {
     Header,
+    Main,
+    Footer,
   },
 };
 </script>
 <style lang="scss">
-@import "./assent/scss/style.scss";
+@import "./assets/scss/style.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
